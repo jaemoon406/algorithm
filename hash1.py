8b2337c85f38d30dd690e5c0525c93bd414c3c62
jaemong <jaemoon406@gmail.com>
2022-03-14 15:39:15 +0900

    해시 1단계

--- hash1.py
+++ hash1.py
@@ -1,14 +1,21 @@
-def solution(participant, completion):
+'''
+수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때,
+ 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+'''
 
+
+def solution(participant, completion):
     participant.sort()
     completion.sort()
 
-    for i in range(len(completion)):
-        if participant[i] != completion[i] : return participant[i]
-        return participant[i]
-
+    leng = len(completion)
+    for i in range(leng):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[leng]
 
 
 participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
-comletion = ["josipa", "filipa", "marina", "nikola"]
-solution(participant, comletion)
+completion = ["josipa", "filipa", "marina", "nikola"]
+solution(participant, completion)
